b7785d814f2636e9fbdcf3aa024c5287ab301c98
joão lucena <84159438+joo68-src@users.noreply.github.com>
2025-11-11 12:27:23 -0300

    add lab14

--- lab14/lib.s
+++ lab14/lib.s
@@ -1,18 +1,21 @@
+.data
+_stack_top: .skip 1024
+_system_time: .word 0
+
 .section .bss:
 .align 4
 isr_stack:                          # fim da pilha
 .skip 1024                          # pilha alocada pra isr 
 isr_stack_end:                      # base da pilha
 
-# espacos alocados
-data:
-_stack_top: .space 1024
-_system_time: .word 0
-
 # comandos do codigo
-text:
+.text
 .set GPT_BASE, 0xFFFF0100
 .set MIDI_BASE, 0xFFFF0300
+
+.globl _start
+.globl play_note
+.globl _system_time
 
 _start:
     # inicializa sp
@@ -118,3 +121,14 @@ main_isr:
     addi sp, sp, 108
     csrrw sp, mscratch, sp
     mret
+
+play_note: 
+    # a0 = ch, a1 = inst ID, a2 = note, a3 = vel, a4 = dur
+    li t0, MIDI_BASE
+    sh a1, 2(t0)
+    sb a2, 4(t0)
+    sb a3, 5(t0)
+    sh a4, 6(t0)
+    sb a0, (t0)
+
+    ret
